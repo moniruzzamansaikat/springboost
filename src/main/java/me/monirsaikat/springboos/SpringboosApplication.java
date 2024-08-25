435b50d0d5a968b0ffdd3b9dddb34a6453b43bcc
moniruzzaman.saikat <saikat.moniruzzaman@gmail.com>
2024-08-26 01:37:14 +0600

    'first'

---
+++ src/main/java/me/monirsaikat/springboos/SpringboosApplication.java
@@ -0,0 +1,13 @@
+package me.monirsaikat.springboos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/monirsaikat/springboos/SpringboosApplicationTests.java
@@ -0,0 +1,13 @@
+package me.monirsaikat.springboos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
